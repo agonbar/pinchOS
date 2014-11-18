ea82ab82b022bdea41c7f1131078cde1613e383f
Adrián González Barbosa <adrian.gonzalez.barbosa@gmail.com>
2014-11-18 18:49:18 +0100

    Participantes
    Added files of Participantes

---
+++ src/consultaPart.php
@@ -0,0 +1,52 @@
+﻿<!DOCTYPE html>
+<?php
+  include "cabecera.php";
+  include "inicio.php"
+?>
+        <div class="margensup" >
+          <div class="column col-lg-10 col-md-10 col-sm-12 col-xs-12 col-md-offset-1" >
+            <div class="modalbox movedown">
+              <div class="row">
+                <h2 class="alineado">
+                  Local X
+                <div><img src="./resources/img/participante.jpg" alt="./resources/img/participante.jpg" class="img-thumbnail" height="200" width="200"></div>
+                </h2>
+              </div>
+              <div class="row separacion" >
+                <div class="column col-lg-10 col-md-10 col-sm-12 col-xs-12 col-md-offset-1">
+                  <form class="form-horizontal separarformulario" role="form">
+                    <div class="form-group alineado ">
+                      <label class="control-label">Calle: </label>
+                      <label class=" control-label">Jose Ramón Goteras, 23</label>
+                    </div>
+                    <div class="form-group alineado ">
+                      <label class="control-label">Email: </label>
+                      <label class=" control-label">somosunbarguay@gmail.com</label>
+                    </div>
+
+                  </form>
+                </div>
+              </div>
+              <div class="row separartabla" >
+                <div class="column col-lg-10 col-md-10 col-sm-12 col-xs-12 col-md-offset-1">
+                  <table class="table alineado ">
+                      <!-- Aplicadas en las filas -->
+                      <tr class="activa">
+                      <td>Pincho</td>
+                      <td>Ingredientes</td>
+                      <td>Chef</td>
+                      </tr>
+                      <tr class="tablehover">
+                      <td>Bocadito de cielo</td>
+                      <td>Jamon</td>
+                      <td>Otilio Manuel</td>
+                      </tr>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+<?php
+  include "pie.php";
+?>
---
+++ src/listarPart.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<?php
+  include "cabecera.php";
+  include "inicio.php"
+?>
+        <!--AQUI EMPIEZA LA VENTANA MODAL DE AÑADIR ALBUM -->
+        <div class="margensup" >
+          <div class="column col-lg-10 col-md-10 col-sm-12 col-xs-12 col-md-offset-1" >
+            <div class="modalbox movedown">
+              <div class="row">
+                <h2 class="alineado">Listado de Participante</h2>
+              </div>
+              <ul class= "list-inline ">
+                <?php //bucle que lista los participantes seleccionados
+                $i=0;
+                while ($i <= 12){?>
+                  <li>
+                      <img src="./resources/img/participante.jpg" alt="./resources/img/participante.jpg" class="img-thumbnail" height="200" width="200">
+                      <div class="caption">
+                        <h4>-Nombre del participante <?php echo $i;?>-</h4>
+                        <p>
+                          <a href="button" class="btn btn-primary col-md-offset-4" role="button">Consultar</a>
+                        </p>
+                      </div>
+                  </li>
+                <?php $i++;}?><!-- fin while-->
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!--AQUI TERMINA LA VENTANA MODAL DE AÑADIR ALBUM -->
+<?php
+  include "pie.php";
+?>
