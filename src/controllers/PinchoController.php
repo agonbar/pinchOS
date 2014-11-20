76e679e3cff427600a19bd7310dd49cf4628cb8b
Melania Sáchez Blanco <melania.sanchez.blanco@gmail.com>
2014-11-20 14:05:30 +0100

    models & controllers folders

---
+++ src/controllers/PinchoController.php
@@ -0,0 +1,26 @@
+<?php
+	//include de la clase Pincho
+	//include de la clase PDO(la que conecta con la base de datos)
+	//include de la clase router(la que direcciona las paginas html)
+	function altaPincho(){
+
+	}
+	function bajaPincho(){
+
+	}
+	function validacionInfo(){
+
+	}
+	function modificacionPincho(){
+		
+	}
+	function busquedaPincho(){
+		
+	}
+	function consultaPincho(){
+		
+	}
+	function listadoPincho(){
+		
+	}
+?>
