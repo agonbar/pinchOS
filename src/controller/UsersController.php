6f1a91aba02e9180a3fbca0bd48cc7d3d8de87f0
jeny093 <jeny-093@hotmail.com>
2014-11-24 17:26:25 +0100

    modificado error en usersController

--- src/controller/UsersController.php
+++ src/controller/UsersController.php
@@ -1,48 +1,87 @@
 <?php
+
 require_once(__DIR__."/../core/ViewManager.php");
-//require_once(__DIR__."/../model/User.php");
-//require_once(__DIR__."/../model/UserMapper.php");
+require_once(__DIR__."/../model/User.php");
 require_once(__DIR__."/../controller/DBController.php");
-/**
- * Class UsersController
- * 
- * Controller to login, logout and user registration
- * 
- * @author lipido <lipido@gmail.com>
- */
+
 class UsersController extends DBController {
-  
-  /**
-   * Reference to the UserMapper to interact
-   * with the database
-   * 
-   * @var UserMapper
-   */  
-  private $userMapper;    
+ 
+  private $user;    
   
   public function __construct() {    
     parent::__construct();
     
-    //$this->userMapper = new UserMapper();
-    // Users controller operates in a "welcome" layout
-    // different to the "default" layout where the internal
-    // menu is displayed
-    //$this->view->setLayout("welcome");     
+    $this->user = new User();
+     //$this->view->setLayout("welcome"); 
   }
 
-  
   public function login() {
-     
-    // render the view (/view/users/login.php)
-    $this->view->render("vistas", "login");    
-  }
   
-   public function registro() {
-     
-    // render the view (/view/users/login.php)
-    $this->view->render("vistas", "registro");    
+   if (isset($_POST["emailU"])){   
+		  if ($this->user->isValidUser($_POST["emailU"], $_POST["contrasenaU"])) {
+
+			$user_db=$this->user->ver_datos($POST["emailU"]);
+			$_SESSION["currentuser"]=$user_db;
+			
+			// envia al usuario a una area restringida (su zona de usuario)
+			$this->view->redirect("posts", "index");  //falta poner bien
+		
+		  }else{
+			$errors = array();
+			$errors["general"] = "El email no se encuentra registrado";
+			$this->view->setVariable("errors", $errors);
+		  }
+    }     
+	
+    // renderiza la vista (/view/users/login.php)
+    $this->view->render("vistas", "login");    //falta poner bien
   }
-  
- 
+
+  public function register() {
+    
+    $user = new User();
+    
+    if (isset($_POST["emailU"])){ 
+      
+      $user->setEmailU($_POST["emailU"]);
+      $user->setContrasenaU($_POST["contrasenaU"]);
+	  $user->setTipoU($_POST["tipoU"]);
+      $user->setEstadoU($_POST["estadoU"]);
+	  $user->setNombreU($_POST["nombreU"]);
+      $user->setConcurso_idC1($_POST["Concurso_idC1"]);
+      
+      try{
+	$user->checkIsValidForRegister(); 
+	
+	// comprueba si el correo ya existe en la base de datos
+	if (!$this->userMapper->usernameExists($_POST["emailU"])){
+	
+	  // guarda el objeto User en la base de datos
+	  $this->userMapper->save($user);
+	  
+	  $this->view->setFlash("Usuario ".$userMapper->getNombreU()." corrrectamente añadido");
+	  
+	  // cabecera("Location: index.php?controller=users&action=login")
+	  
+	  $this->view->redirect("users", "login");	   //falta poner bien
+	} else {
+	  $errors = array();
+	  $errors["emailU"] = "El email ya se encuentra registrado";
+	  $this->view->setVariable("errors", $errors);
+	}
+      }catch(ValidationException $ex) {
+	
+	$errors = $ex->getErrors();
+
+	$this->view->setVariable("errors", $errors);
+      }
+    } 
+    $this->view->setVariable("user", $user);  
+    
+    // renderiza la vista (/view/users/registro.php)
+    $this->view->render("vistas", "registro"); 
+    
+  }
+
   
 }
