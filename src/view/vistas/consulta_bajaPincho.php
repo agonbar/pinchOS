0f91c08ecb59837bbca2e7905b996b025dd6c67d
Adrian Gonzalez Barbosa <adrian.gonzalez.barbosa@gmail.com>
2014-11-27 09:49:45 +0100

    Missing includes

--- src/view/vistas/consulta_bajaPincho.php
+++ src/view/vistas/consulta_bajaPincho.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <?php
 include(__DIR__."/../layouts/inicio.php");
+require_once(__DIR__."/../../core/ViewManager.php");
+$view = ViewManager::getInstance();
+$view->setVariable("title", "Login");
+$errors = $view->getVariable("errors");
+$concurso = $view->getVariable("concu");
 ?>
 <!--AQUI EMPIEZA LA VENTANA MODAL DE AÑADIR ALBUM -->
 <div class="margensup" >
