65c9e19c2afee59879fdc7574b8324b40af1561c
Adrian Gonzalez Barbosa <adrian.gonzalez.barbosa@gmail.com>
2014-11-27 13:44:09 +0100

    Fixed

--- src/model/Participante.php
+++ src/model/Participante.php
@@ -13,10 +13,12 @@ class Participante {
     $stmt->execute();
     $users_db = $stmt->fetch_all(PDO::FETCH_ASSOC);
 
-    if(empty($user_db)){
-      return null;
-    }else{
-      return $users_db;
+    $participantes=array();
+    foreach ($users_db as $participante) {
+      array_push($participantes, new User($emailU["email"], $contrasenaU["constrasena"], $tipoU["tipo"], $estadoU["estado"], $nombreU=["nombre"], $concursoId=["concurso"]);
+    }
+    
+    return $participantes;
     }
   }
 }
