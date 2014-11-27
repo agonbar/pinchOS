3624fec822fb886ee017652d7c07d7f0bfa59412
Adrian Gonzalez Barbosa <adrian.gonzalez.barbosa@gmail.com>
2014-11-27 09:46:15 +0100

    Fixed include order

--- src/controller/PinchoController.php
+++ src/controller/PinchoController.php
@@ -1,6 +1,6 @@
 <?php
-require_once(__DIR__."/../core/ViewManager.php");
 require_once(__DIR__."/../model/User.php");
+require_once(__DIR__."/../core/ViewManager.php");
 require_once(__DIR__."/../controller/DBController.php");
 
 
