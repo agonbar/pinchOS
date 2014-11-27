2962c6c274f723748ca7fca0c9e8eb86e7d9f28b
Adrian Gonzalez Barbosa <adrian.gonzalez.barbosa@gmail.com>
2014-11-27 11:55:06 +0100

    Begin implementation os Participante controller

---
+++ src/model/Participante.php
@@ -0,0 +1,18 @@
+<?php
+require_once(__DIR__."/../core/PDOConnection.php");
+require_once(__DIR__."/../core/ValidationException.php");
+
+class Participante {
+  public function listar(){
+    $db = PDOConnection::getInstance();
+    $stmt = $db->prepare("SELECT * FROM usuario where tipoU=P");
+    $stmt->execute();
+    $users_db = $stmt->fetch_all(PDO::FETCH_ASSOC);
+
+    if(empty($user_db)){
+      return null;
+    }else{
+      return $users_db;
+    }
+  }
+}
