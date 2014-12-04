5a920018297bb37e213b175cfc32d3dd4d2c42bf
jeny093 <jeny-093@hotmail.com>
2014-12-04 10:52:39 +0100

    cambios

--- src/view/vistas/altaConcurso.php
+++ src/view/vistas/altaConcurso.php
@@ -66,7 +66,7 @@ $errors = $view->getVariable("errors");
 					<label class="col-lg-2 col-md-2 col-sm-2 col-xs-12 control-label">Bases</label>
 					<div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
 						<textarea class="form-control" rows="6" placeholder="Introduce las Bases..." name="basesC"></textarea>
-						<input type="file" id="archivo_1">
+						<input type="file" id="archivo_1" name="basesC">
 						<?= isset($errors["basesC"])?$errors["basesC"]:"" ?><br>
 					</div>
 				</div>
