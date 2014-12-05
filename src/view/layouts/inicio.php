e8b7262907846348b6daeb666b3f920d521d149c
Adrián González Barbosa <adrian.gonzalez.barbosa@gmail.com>
2014-12-05 18:57:38 +0100

    Deleted unused serchbar

--- src/view/layouts/inicio.php
+++ src/view/layouts/inicio.php
@@ -35,99 +35,91 @@ error_reporting(E_ALL);
 							</a>
 							<ul class="list-unstyled collapse" id="menu1">
 								<?php if ($currentuser->getTipoU() == 'A') { ?>
-								<li class="desplegable "><a href="index.php?controller=concurso&action=registro">Crear concurso</a></li>
-								<li class="desplegable"><a href="index.php?controller=concurso&action=modificarConcurso">Modificar concurso</a></li>
-								<?php } ?>
-								<li class="desplegable "><a href="index.php?controller=concurso&action=consultarConcurso">Consultar concurso</a></li>
-							</ul>
-						</li>
-
-						<li class="nav-header">
-							<a href="#" data-toggle="collapse" data-target="#menu2">
-								<h5>Participantes <i class="glyphicon glyphicon-plus"></i></h5>
-							</a>
-							<ul class="list-unstyled collapse" id="menu2">
-								<li class="desplegable"><a href="index.php?controller=participante&action=listarParticipantes">Listado</a></li>
-								<li class="desplegable"><a href="index.php?controller=participante&action=busquedaParticipante">Búsqueda</a></li>
-							</ul>
-						</li>
-
-						<li class="nav-header">
-							<a href="#" data-toggle="collapse" data-target="#menu3">
-								<h5>Pinchos <i class="glyphicon glyphicon-plus"></i></h5>
-							</a>
-							<ul class="list-unstyled collapse" id="menu3">
-								<li class="desplegable"><a href="index.php?controller=pincho&action=listadoPincho">Listado pincho</a></li>
-								<li class="desplegable"><a href="index.php?controller=pincho&action=altaPincho">Alta pincho</a></li>
-								<li class="desplegable"><a href="index.php?controller=pincho&action=busquedaPincho">Búsqueda pincho</a></li>
-							</ul>
-						</li>
-
-						<?php if ($currentuser->getTipoU() == 'A') { ?>
-						<li class="nav-header">
-							<a href="#" data-toggle="collapse" data-target="#menu5">
-								<h5>Jurado Profesional <i class="glyphicon glyphicon-plus"></i></h5>
-							</a>
-							<ul class="list-unstyled collapse" id="menu5">
-								<li class="desplegable "><a href="index.php?controller=profesional&action=registrarProfesional">Crear J.Profesional</a></li>
-							</ul>
-						</li>
-						<?php } ?>
-					</ul>
-
-				</div>
-				<!-- /sidebar -->
-
-				<!-- main right col -->
-				<div class="column col-sm-10 col-xs-11" id="main">
-
-					<!-- top nav -->
-					<div class="navbar navbar-blue navbar-static-top">
-						<div class="navbar-header">
-							<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-								<span class="sr-only">Toggle</span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							</button>
-							<a href="index.php?controller=concurso&action=consultarConcurso" class="navbar-brand logo">P</a>
-						</div>
-						<nav class="collapse navbar-collapse" role="navigation">
-							<form class="navbar-form navbar-left">
-								<div class="input-group input-group-sm" style="max-width:360px;">
-									<input type="text" class="form-control" placeholder="Buscar" name="srch-term" id="srch-term">
-									<div class="input-group-btn">
-										<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-									</div>
-								</div>
-							</form>
-							<ul class="nav navbar-nav">
-								<li>
-									<a href="index.php?controller=pincho&action=listarPrem"><i class="glyphicon glyphicon-plus"></i> Premiados</a>
-								</li>
-								<li>
-									<?php if (($currentuser->getTipoU() == 'J') or ($currentuser->getTipoU() == 'S')) { ?>
-									<a href="index.php?controller=users&action=seleccionarVotacion"><i class="glyphicon glyphicon-plus"></i> Votar</a>
+									<li class="desplegable "><a href="index.php?controller=concurso&action=registro">Crear concurso</a></li>
+									<li class="desplegable"><a href="index.php?controller=concurso&action=modificarConcurso">Modificar concurso</a></li>
 									<?php } ?>
-								</li>
-								<li>
-									<?php if ($currentuser->getTipoU() == 'A') { ?>
-										<a href="index.php?controller=pincho&action=cerrarVotacion"><i class="glyphicon glyphicon-plus"></i> Cerrar concurso</a>
-										<?php } ?>
-									</li>
-							</ul>
-							<ul class="nav navbar-nav navbar-right">
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-cog"></i></a>
-									<ul class="dropdown-menu">
-										<?php if ($currentuser->getTipoU() != 'A' && $currentuser->getTipoU() != 'P') { ?>
-										<li><a href="index.php?controller=users&action=seleccionarPerfil">Mi perfil</a></li>
-										<li><a href="index.php?controller=users&action=seleccionarModificacion">Modificar mi perfil</a></li>
-										<?php } ?>
-										<li><a href="index.php?controller=users&action=logout">Salir</a></li>
+									<li class="desplegable "><a href="index.php?controller=concurso&action=consultarConcurso">Consultar concurso</a></li>
+								</ul>
+							</li>
+
+							<li class="nav-header">
+								<a href="#" data-toggle="collapse" data-target="#menu2">
+									<h5>Participantes <i class="glyphicon glyphicon-plus"></i></h5>
+								</a>
+								<ul class="list-unstyled collapse" id="menu2">
+									<li class="desplegable"><a href="index.php?controller=participante&action=listarParticipantes">Listado</a></li>
+									<li class="desplegable"><a href="index.php?controller=participante&action=busquedaParticipante">Búsqueda</a></li>
+								</ul>
+							</li>
+
+							<li class="nav-header">
+								<a href="#" data-toggle="collapse" data-target="#menu3">
+									<h5>Pinchos <i class="glyphicon glyphicon-plus"></i></h5>
+								</a>
+								<ul class="list-unstyled collapse" id="menu3">
+									<li class="desplegable"><a href="index.php?controller=pincho&action=listadoPincho">Listado pincho</a></li>
+									<li class="desplegable"><a href="index.php?controller=pincho&action=altaPincho">Alta pincho</a></li>
+									<li class="desplegable"><a href="index.php?controller=pincho&action=busquedaPincho">Búsqueda pincho</a></li>
+								</ul>
+							</li>
+
+							<?php if ($currentuser->getTipoU() == 'A') { ?>
+								<li class="nav-header">
+									<a href="#" data-toggle="collapse" data-target="#menu5">
+										<h5>Jurado Profesional <i class="glyphicon glyphicon-plus"></i></h5>
+									</a>
+									<ul class="list-unstyled collapse" id="menu5">
+										<li class="desplegable "><a href="index.php?controller=profesional&action=registrarProfesional">Crear J.Profesional</a></li>
 									</ul>
 								</li>
+								<?php } ?>
 							</ul>
-						</nav>
-					</div>
-					<!-- /top nav -->
+
+						</div>
+						<!-- /sidebar -->
+
+						<!-- main right col -->
+						<div class="column col-sm-10 col-xs-11" id="main">
+
+							<!-- top nav -->
+							<div class="navbar navbar-blue navbar-static-top">
+								<div class="navbar-header">
+									<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+										<span class="sr-only">Toggle</span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+									</button>
+									<a href="index.php?controller=concurso&action=consultarConcurso" class="navbar-brand logo">P</a>
+								</div>
+								<nav class="collapse navbar-collapse" role="navigation">
+									<ul class="nav navbar-nav">
+										<li>
+											<a href="index.php?controller=pincho&action=listarPrem"><i class="glyphicon glyphicon-plus"></i> Premiados</a>
+										</li>
+										<li>
+											<?php if (($currentuser->getTipoU() == 'J') or ($currentuser->getTipoU() == 'S')) { ?>
+												<a href="index.php?controller=users&action=seleccionarVotacion"><i class="glyphicon glyphicon-plus"></i> Votar</a>
+												<?php } ?>
+											</li>
+											<li>
+												<?php if ($currentuser->getTipoU() == 'A') { ?>
+													<a href="index.php?controller=pincho&action=cerrarVotacion"><i class="glyphicon glyphicon-plus"></i> Cerrar concurso</a>
+													<?php } ?>
+												</li>
+											</ul>
+											<ul class="nav navbar-nav navbar-right">
+												<li class="dropdown">
+													<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-cog"></i></a>
+													<ul class="dropdown-menu">
+														<?php if ($currentuser->getTipoU() != 'A' && $currentuser->getTipoU() != 'P') { ?>
+															<li><a href="index.php?controller=users&action=seleccionarPerfil">Mi perfil</a></li>
+															<li><a href="index.php?controller=users&action=seleccionarModificacion">Modificar mi perfil</a></li>
+															<?php } ?>
+															<li><a href="index.php?controller=users&action=logout">Salir</a></li>
+														</ul>
+													</li>
+												</ul>
+											</nav>
+										</div>
+										<!-- /top nav -->
