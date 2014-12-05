6f7e463242c11d3de143e990341b270b6ca08bef
Adrián González Barbosa <adrian.gonzalez.barbosa@gmail.com>
2014-12-05 23:29:28 +0100

    LAst commit

--- src/model/User.php
+++ src/model/User.php
@@ -184,6 +184,8 @@ class User {
     $db = PDOConnection::getInstance();
     $stmt = $db->prepare("INSERT INTO usuario values (?,?,?,?,?,?)");
     $stmt->execute(array($this->emailU, $this->contrasenaU, $this->tipoU, $this->estadoU, $this->nombreU, $this->concursoId));
+    $stmt-> $db->prepare("INSERT INTO participante values usuarioEmail = ?");
+    $stmt->execute(array($this->emailU));
   }
 
   /* Actualiza el User en la base de datos,para ello se hace un update de los distintos campos que contiene la tabla usuario*/
