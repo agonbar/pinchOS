1c59b9eb041d09198382a4fb9b99ffecf10ea65e
Adrián González Barbosa <adrian.gonzalez.barbosa@gmail.com>
2014-12-15 17:33:04 +0100

    Moved js to correct place

--- src/view/vistas/portada.php
+++ src/view/vistas/portada.php
@@ -82,12 +82,12 @@ include("./view/layouts/cabecera.php");
   <!-- Bootstrap core JavaScript
   ================================================== -->
   <!-- Placed at the end of the document so the pages load faster -->
-  <script src="assets/js/jquery.min.js"></script>
-  <script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
-  <script src="assets/js/soon/plugins.js"></script>
-  <script src="assets/js/soon/jquery.themepunch.revolution.min.js"></script>
-  <script src="assets/js/soon/custom.js"></script>
+  <script src="resources/js/jquery.min.js"></script>
+  <script type="text/javascript" src="resources/js/modernizr.custom.js"></script>
+  <script src="resources/js/bootstrap.min.js"></script>
+  <script src="resources/js/soon/plugins.js"></script>
+  <script src="resources/js/soon/jquery.themepunch.revolution.min.js"></script>
+  <script src="resources/js/soon/custom.js"></script>
 </body>
 <!-- END BODY -->
 </html>
