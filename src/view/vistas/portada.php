fa1f2ee2348266cca5da3d76c1b33f02e234a2e9
Adrián González Barbosa <adrian.gonzalez.barbosa@gmail.com>
2014-12-15 17:30:08 +0100

    Moved css and fonts to correct place
    Still needs to move javascript

--- src/view/vistas/portada.php
+++ src/view/vistas/portada.php
@@ -1,33 +1,12 @@
 <?php
 require_once(__DIR__."/../../core/ViewManager.php");
 $view = ViewManager::getInstance();
+include("./view/layouts/cabecera.php");
 ?>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <link rel="shortcut icon" href="assets/ico/favicon.png">
 
-  <title>Consurso de pinchos</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="assets/css/bootstrap.css" rel="stylesheet">
-  <link href="./resources/css/styles.css" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="assets/css/soon.css" rel="stylesheet">
-
-
-  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!--[if lt IE 9]>
-  <script src="assets/js/html5shiv.js"></script>
-  <script src="assets/js/respond.min.js"></script>
-  <![endif]-->
-
-  <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
-  <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+  <link href="./resources/css/soon.css" rel="stylesheet">
 
 </head>
 <!-- START BODY -->
